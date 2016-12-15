abca3cc37427c88313e93e8ca0bdc920a646cfec
Adrian Szymański <aszymanski@infusion.com>
2016-12-15 11:16:04 +0100

    Add project files.

---
+++ SampleSpecFlowProject/features/CardValidation.feature
@@ -0,0 +1,12 @@
+﻿@Sample
+Feature: CardValidation
+
+Scenario: Credit Card Validation - number too short
+	Given I open application 
+	When I enter credit card number "999222333888444"
+	Then I can see validation failure
+
+Scenario: Credit Card Validation - number too short 2
+	Given I open application 
+	When I enter credit card number "999222333888224"
+	Then I can see validation failure
---
+++ SampleSpecFlowProject/stepDefinitions/CardValidationSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+namespace SampleSpecFlowProject.stepDefinitions
+{
+    [Binding]
+    public class CardValidationSteps
+    {
+        AndroidApp app;
+
+        [BeforeScenario]
+        public void SetUp()
+        {
+            app = ConfigureApp.Android
+                .ApkFile("D:/com.xamarin.example.creditcardvalidator.apk")
+                .StartApp();
+        }
+
+        [Given(@"I open application")]
+        public void GivenIOpenApplication()
+        {
+           app.WaitForElement(c => c.Marked("action_bar_title").Text("Enter Credit Card Number"));
+        }
+
+
+        [When(@"I enter credit card number ""(.*)""")]
+        public void GivenIEnterCreditCardNumber(string creditCardNumber)
+        {
+            app.EnterText(c => c.Marked("creditCardNumberText"), creditCardNumber);
+            app.Tap(c => c.Marked("validateButton"));
+        }
+        [Then(@"I can see validation failure")]
+        public void ThenICanSeeValidationFailure()
+        {
+            app.WaitForElement(c => c.Marked("errorMessagesText").Text("Credit card number is too short."));
+        }
+
+    }
+}
